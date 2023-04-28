694ef2c21fd5c8b9a40208e9f53bb09169b6be88
Luiz Coelho <luizneto0213@gmail.com>
2023-04-28 00:45:53 -0300

    style: Add good practices in the code

--- src/pages/movieView/index.jsx
+++ src/pages/movieView/index.jsx
@@ -33,6 +33,8 @@ export default function MovieView(props) {
   const IMG_URL = "https://image.tmdb.org/t/p/w500/";
   const MOVIE_URL = `https://api.themoviedb.org/3/movie/${MOVIE_ID}?api_key=440d2f7f4782ff80872b983192a83cf8&language=pt-BR`;
 
+  console.log(dataMovie);
+
   useEffect(() => {
     axios.get(MOVIE_URL).then((response) => {
       setDataMovie(response.data);
@@ -53,7 +55,7 @@ export default function MovieView(props) {
               <span className="absolute h-full w-full bg-gradient-to-t from-[#15161C]"></span>
             </div>
           </div>
-          <div className="col-span-3 flex flex-col justify-between gap-10">
+          <div className="col-span-3 mx-5 flex flex-col justify-between gap-10">
             <div className="space-y-2">
               <h2 className="font-semibold">{dataMovie.title}</h2>
               <div className="flex gap-10">
@@ -79,35 +81,38 @@ export default function MovieView(props) {
 
             <div className="flex flex-wrap items-center gap-4">
               <h5 className="font-semibold">Genêros: </h5>
-              {dataMovie.genres.map((item) => (
-                <small className="rounded-lg border border-green-600 bg-green-900 px-2 py-1">
+              {dataMovie.genres.map((item, index) => (
+                <small
+                  key={index}
+                  className="rounded-lg border border-green-600 bg-green-900 px-2 py-1"
+                >
                   {item.name}
                 </small>
               ))}
             </div>
             <div className="flex flex-wrap items-center gap-4">
               <h5 className="font-semibold">Produtoras: </h5>
-              {dataMovie.production_companies.map((item) => (
-                <small className="rounded-lg border border-zinc-600 bg-zinc-900 px-2 py-1">
+              {dataMovie.production_companies.map((item, index) => (
+                <small
+                  key={index}
+                  className="rounded-lg border border-zinc-600 bg-zinc-900 px-2 py-1"
+                >
                   {item.name}
                 </small>
               ))}
             </div>
             <div className="flex flex-wrap items-center gap-4">
               <h5 className="font-semibold">Países de produção: </h5>
-              {dataMovie.production_countries.map((item) => (
-                <small className="rounded-lg border border-zinc-600 bg-zinc-900 px-2 py-1">
+              {dataMovie.production_countries.map((item, index) => (
+                <small
+                  key={index}
+                  className="rounded-lg border border-zinc-600 bg-zinc-900 px-2 py-1"
+                >
                   {item.name}
                 </small>
               ))}
             </div>
             <div className="grid grid-cols-3 gap-10">
-              <div className="space-y-1">
-                <h5 className="font-semibold">Lançamento</h5>
-              </div>
-              <div className="space-y-1">
-                <h5 className="font-semibold">Avaliação</h5>
-              </div>
               <div className="space-y-1">
                 <h5 className="font-semibold">Votos</h5>
                 <div className="flex items-center gap-2">
@@ -115,9 +120,7 @@ export default function MovieView(props) {
                   <p>{dataMovie.vote_count}</p>
                 </div>
               </div>
-              <div className="space-y-1">
-                <h5 className="font-semibold">Duração</h5>
-              </div>
+
               <div className="space-y-1">
                 <h5 className="font-semibold">Orçamento</h5>
 
@@ -136,7 +139,7 @@ export default function MovieView(props) {
           <div className="col-span-1 flex justify-center">
             <input type="checkbox" id="choose-me" className="peer hidden" />
             <label
-              for="choose-me"
+              htmlFor="choose-me"
               className="flex h-fit cursor-pointer select-none items-center gap-2 rounded-lg border-2 border-gray-200 p-2 font-bold text-gray-200 transition-colors duration-200 ease-in-out peer-checked:border-[#D20939] peer-checked:bg-[#D20939] peer-checked:text-gray-900 "
             >
               <AiFillHeart className="h-5 w-5" /> Favoritar
