7f99a9c117617296a2dbf1f1c617c2ec13e08cbb
Luiz Coelho <luizneto0213@gmail.com>
2023-04-28 00:44:23 -0300

    refactor: Change where the API will be called

--- src/components/cardMovie/index.jsx
+++ src/components/cardMovie/index.jsx
@@ -1,28 +1,18 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 import { AiOutlineStar, AiOutlineCalendar } from "react-icons/ai";
 import { TbLanguage } from "react-icons/tb";
 
 const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 
-const MOVIE_URL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=440d2f7f4782ff80872b983192a83cf8&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
-
 export default function CardMovie(props) {
-  const [dataMovie, setDataMovie] = useState([]);
-
-  useEffect(() => {
-    axios.get(MOVIE_URL).then((response) => {
-      setDataMovie(response.data.results.slice(0, -2));
-      console.log(response.data);
-    });
-  }, []);
-
   return (
     <>
-      {dataMovie.map((item) => (
-        <div className="flex gap-5 rounded bg-[#1e1f28] p-5 text-white">
+      {props.data.map((item, index) => (
+        <div
+          key={index}
+          className="flex gap-5 rounded bg-[#1e1f28] p-5 text-white"
+        >
           <img
             className="w-2/5 rounded-xl"
             src={IMG_URL + item.poster_path}
@@ -47,8 +37,11 @@ export default function CardMovie(props) {
                 </div>
               </div>
               <div className="flex flex-wrap gap-4">
-                {item.genre_ids.map((item) => (
-                  <small className="rounded-lg border border-green-600 bg-green-900 px-2 py-1">
+                {item.genre_ids.map((item, index) => (
+                  <small
+                    key={index}
+                    className="rounded-lg border border-green-600 bg-green-900 px-2 py-1"
+                  >
                     Gen {item}
                   </small>
                 ))}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import CardMovie from "../../components/cardMovie";
 
-export default function HomePage(props) {
+const MOVIE_URL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=440d2f7f4782ff80872b983192a83cf8&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+
+export default function HomePage() {
+  const [dataMovie, setDataMovie] = useState([]);
+
+  useEffect(() => {
+    axios.get(MOVIE_URL).then((response) => {
+      setDataMovie(response.data.results.slice(0, -2));
+    });
+  }, []);
+
   return (
     <div className="container mx-auto flex h-auto flex-col gap-10 py-28">
       <h2 className="font-semibold text-white">Featured</h2>
       <div className="grid grid-cols-3 justify-items-center gap-5">
-        <CardMovie />
+        <CardMovie data={dataMovie} />
       </div>
     </div>
   );
