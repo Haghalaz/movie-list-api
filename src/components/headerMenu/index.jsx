fa60af1ae19b731a80b411dbb1302d99b4597f61
Luiz Coelho <luizneto0213@gmail.com>
2023-05-04 23:51:53 -0300

    feat: Add a verification if the user is logged

--- src/components/headerMenu/index.jsx
+++ src/components/headerMenu/index.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import { TbUserCircle } from "react-icons/tb";
+
 export default function HeaderMenu(props) {
+  const [isLogged] = useState(
+    JSON.parse(localStorage.getItem("logged")).logged
+  );
+
   return (
     <header className="body-font absolute w-full bg-[#1E1F28] text-white shadow-sm">
       <div className="container mx-auto flex justify-between p-6 ">
@@ -6,10 +13,17 @@ export default function HeaderMenu(props) {
           TIPFLIX
         </a>
 
-        <div className="ml-6 h-full items-center pl-6">
-          <a href="Login" className="mr-5 font-medium hover:text-[#8CD955]">
-            Login
-          </a>
+        <div className="ml-6 flex h-full items-center pl-6">
+          {isLogged ? (
+            <a href="User" className="mr-5 font-medium hover:text-[#8CD955]">
+              <TbUserCircle className="h-6 w-6" />
+            </a>
+          ) : (
+            <a href="Login" className="mr-5 font-medium hover:text-[#8CD955]">
+              Login
+            </a>
+          )}
+
           <a
             href="SurpriseMe"
             className="ease rounded bg-[#3E8C32] px-4 py-2 text-xs font-bold uppercase text-white shadow outline-none transition-all duration-150 hover:bg-[#8CD955] hover:shadow-md focus:outline-none active:bg-teal-600"
