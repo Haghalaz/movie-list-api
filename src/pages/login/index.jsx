b15fa62f95b054d000cef2551732b2f850c2e50a
Luiz Coelho <luizneto0213@gmail.com>
2023-05-04 23:49:13 -0300

    style: Reorganize the css classes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,42 +5,42 @@ import { AiOutlineMail } from "react-icons/ai";
 
 export default function Login(props) {
   return (
-    <div className="h-full flex items-center justify-center">
-      <div className="bg-[#2C2C37] lg:w-5/12 md:6/12 w-10/12 shadow-3xl">
-        <div className="bg-gray-700 text-white absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4">
+    <div className="flex h-full items-center justify-center">
+      <div className="md:6/12 shadow-3xl w-10/12 bg-[#2C2C37] lg:w-5/12">
+        <div className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-gray-700 p-4 text-white">
           <AiOutlineMail />
         </div>
-        <div className="flex flex-col pb-10 justify-between">
+        <div className="flex flex-col justify-between pb-10">
           <form className="p-12 md:p-24">
-            <div className="flex items-center text-lg mb-6 md:mb-8">
+            <div className="mb-6 flex items-center text-lg md:mb-8">
               <AiOutlineUser className="absolute ml-3" />
               <input
                 type="text"
                 id="username"
-                className="bg-gray-200 rounded-lg pl-12 py-2 md:py-4 focus:outline-none w-full"
-                placeholder="Username"
+                className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
+                placeholder="Usuário"
               />
             </div>
-            <div className="flex items-center text-lg mb-6 md:mb-8">
+            <div className="mb-6 flex items-center text-lg md:mb-8">
               <AiOutlineLock className="absolute ml-3" />
               <input
                 type="password"
                 id="password"
-                className="bg-gray-200 rounded-lg pl-12 py-2 md:py-4 focus:outline-none w-full"
-                placeholder="Password"
+                className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
+                placeholder="Senha"
               />
             </div>
-            <button className="bg-[#3E8C32] font-medium p-2 md:p-4 text-white uppercase w-full">
+            <button className="w-full bg-[#3E8C32] p-2 font-medium uppercase text-white md:p-4">
               Login
             </button>
           </form>
-          <p class="flex flex-col items-center justify-center text-center text-md text-gray-500">
-            <span>Don't have an account?</span>
+          <p class="text-md flex flex-col items-center justify-center text-center text-gray-500">
+            <span>Não tem uma conta?</span>
             <a
               href="/Register"
-              class="text-[#3E8C32] hover:text-[#8CD955] cursor-pointer transition ease-in duration-300"
+              class="cursor-pointer text-[#3E8C32] transition duration-300 ease-in hover:text-[#8CD955]"
             >
-              Sign up
+              Registrar
             </a>
           </p>
         </div>
