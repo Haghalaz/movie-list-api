799bba818f067a31ab29b7c417bef1f272491fd8
Luiz Coelho <luiz.adalberto@selettra.com.br>
2023-08-07 16:13:34 -0300

    feat: Add the verification of a logged user

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import HeaderMenu from "./components/headerMenu";
 import Rotas from "./routes/routes";
 
 function App() {
+  const isLogged = JSON.parse(localStorage.getItem("logged"));
+
+  if (!isLogged) {
+    let initUser = { logged: false };
+    localStorage.setItem("logged", JSON.stringify(initUser));
+    localStorage.setItem("userId", "00000000-0000-0000-0000-000000000000");
+  }
+
   return (
     <div className="h-screen bg-[#15161C]">
       <HeaderMenu></HeaderMenu>
