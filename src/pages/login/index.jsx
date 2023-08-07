0254f995dbe70a533479614a0708a0c17f93ad59
Luiz Coelho <luiz.adalberto@selettra.com.br>
2023-08-07 16:18:21 -0300

    feat: Integrate the backend user control

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,29 @@
-// Icons
-import { AiOutlineUser } from "react-icons/ai";
-import { AiOutlineLock } from "react-icons/ai";
-import { AiOutlineMail } from "react-icons/ai";
+import axios from "axios";
+import { useRef } from "react";
+import { AiOutlineLock, AiOutlineMail, AiOutlineUser } from "react-icons/ai";
 
-export default function Login(props) {
+export default function Login() {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  async function loginRequest() {
+    const EMAIL = emailInput.current.value;
+    const PASSWORD = passwordInput.current.value;
+
+    try {
+      var request = `${process.env.REACT_APP_USER_LOGIN}?email=${EMAIL}&password=${PASSWORD}`;
+      const response = await axios.get(request);
+
+      let initUser = { logged: true };
+      let userId = response.data.id;
+      localStorage.setItem("logged", JSON.stringify(initUser));
+      localStorage.setItem("userId", userId);
+      window.location.pathname = "/";
+    } catch (error) {
+      alert("Erro: Tente novamente");
+    }
+  }
+
   return (
     <div className="flex h-full items-center justify-center">
       <div className="md:6/12 shadow-3xl w-10/12 bg-[#2C2C37] lg:w-5/12">
@@ -17,6 +37,7 @@ export default function Login(props) {
               <input
                 type="text"
                 id="username"
+                ref={emailInput}
                 className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
                 placeholder="Usuário"
               />
@@ -26,19 +47,26 @@ export default function Login(props) {
               <input
                 type="password"
                 id="password"
+                ref={passwordInput}
                 className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
                 placeholder="Senha"
               />
             </div>
-            <button className="w-full bg-[#3E8C32] p-2 font-medium uppercase text-white md:p-4">
+            <button
+              className="w-full bg-[#3E8C32] p-2 font-medium uppercase text-white md:p-4"
+              onClick={(evt) => {
+                evt.preventDefault();
+                loginRequest();
+              }}
+            >
               Login
             </button>
           </form>
-          <p class="text-md flex flex-col items-center justify-center text-center text-gray-500">
+          <p className="text-md flex flex-col items-center justify-center text-center text-gray-500">
             <span>Não tem uma conta?</span>
             <a
               href="/Register"
-              class="cursor-pointer text-[#3E8C32] transition duration-300 ease-in hover:text-[#8CD955]"
+              className="cursor-pointer text-[#3E8C32] transition duration-300 ease-in hover:text-[#8CD955]"
             >
               Registrar
             </a>
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,35 @@
+import { useRef } from "react";
+import uuid from "react-uuid";
+import axios from "axios";
+
 // Icons
 import { AiOutlineUser, AiOutlineMail, AiOutlineLock } from "react-icons/ai";
 
 export default function Register() {
+  const usernameInput = useRef(null);
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  async function registerRequest(num) {
+    const USERNAME = usernameInput.current.value;
+    const EMAIL = emailInput.current.value;
+    const PASSWORD = passwordInput.current.value;
+
+    const JSON_REGISTER = {
+      id: uuid(),
+      name: USERNAME,
+      email: EMAIL,
+      password: PASSWORD,
+      movies: [],
+    };
+    try {
+      await axios.post(process.env.REACT_APP_USER_REGISTER, JSON_REGISTER);
+      window.location.pathname = "/";
+    } catch (error) {
+      alert("Erro: Tente novamente");
+    }
+  }
+
   return (
     <div className="flex h-full items-center justify-center">
       <div className="md:6/12 shadow-3xl w-10/12 bg-[#2C2C37] lg:w-5/12">
@@ -15,6 +43,7 @@ export default function Register() {
               <input
                 type="text"
                 id="User"
+                ref={usernameInput}
                 className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
                 placeholder="Nome de usuário"
               />
@@ -24,6 +53,7 @@ export default function Register() {
               <input
                 type="email"
                 id="email"
+                ref={emailInput}
                 className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
                 placeholder="E-mail"
               />
@@ -33,28 +63,26 @@ export default function Register() {
               <input
                 type="password"
                 id="senha"
+                ref={passwordInput}
                 className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
                 placeholder="Senha"
               />
             </div>
-            <div className="mb-6 flex items-center text-lg md:mb-8">
-              <AiOutlineLock className="absolute ml-3" />
-              <input
-                type="password"
-                id="password"
-                className="w-full rounded-lg bg-gray-200 py-2 pl-12 focus:outline-none md:py-4"
-                placeholder="Repita a senha"
-              />
-            </div>
-            <button className="w-full bg-[#3E8C32] p-2 font-medium uppercase text-white md:p-4">
+            <button
+              className="w-full bg-[#3E8C32] p-2 font-medium uppercase text-white md:p-4"
+              onClick={(evt) => {
+                evt.preventDefault();
+                registerRequest();
+              }}
+            >
               Registrar
             </button>
           </form>
-          <p class="text-md flex flex-col items-center justify-center text-center text-gray-500">
+          <p className="text-md flex flex-col items-center justify-center text-center text-gray-500">
             <span>Já tem uma conta?</span>
             <a
               href="/Login"
-              class="cursor-pointer text-[#3E8C32] transition duration-300 ease-in hover:text-[#8CD955]"
+              className="cursor-pointer text-[#3E8C32] transition duration-300 ease-in hover:text-[#8CD955]"
             >
               Login
             </a>
